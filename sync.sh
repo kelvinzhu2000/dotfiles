1c1291a5cb93c0df5226262de08065acc29755de
kelvinzhu2000 <kelvinzhu2000@yahoo.com>
2012-06-15 15:51:53 -0700

    Returned sync.sh to working state

--- sync.sh
+++ sync.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-echo "$( dirname $0 )"
-DIR="$( dirname $0 )"
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 rm -rf $DIR/.oh-my-zsh/themes/bunsen.zsh-theme
 cp $DIR/bunsen.zsh-theme $DIR/.oh-my-zsh/themes/
